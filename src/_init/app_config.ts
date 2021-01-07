4443f2b0dbcc946df63bf176c7825170bfe58c93
dongbo <895188445@qq.com>
2021-01-07 16:13:58 +0800

    .

--- src/_init/app_config.ts
+++ src/_init/app_config.ts
@@ -2,16 +2,15 @@ export interface IurlConfig {
     api: string;
 }
 export interface IappConfig {
-    readonly current_url_config?: IurlConfig;
+    readonly urls?: IurlConfig;
     domain?: string;
 }
 export type EnvType = "prod" | "test" | "dev";
+
+
 /**
  * APP的配置
  */
 export const app_config: IappConfig = {
     domain: "target_domain"
 }
-
-
-
