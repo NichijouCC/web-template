4a5c60a17d98bc628495cb962ad5342eb049495c
dongbo <895188445@qq.com>
2021-06-09 10:01:05 +0800

    update(store):webstore修改为sessionstorage。 原因：利用localstore+refcount的方案下，有时-refcount事件没有触发

--- src/__internal/webStore.ts
+++ src/__internal/webStore.ts
@@ -1,14 +1,13 @@
 import { debuglog } from "./util";
 import { EventEmitter, EventTarget } from "@mtgoo/ctool"
 /**
- * 可在同源界面同步的store
+ * 有事件的sessionStorage
  */
 export class WebStore extends EventEmitter<{ "webStore:set": { prop: string, newValue: any, oldValue: any } }> {
     private _data: any = {};
     private static privateStore = "__webStore:data";
     private static storeSetEvent = "__webStore:set";
     private static storeRemoveEvent = "__webStore:remove";
-    private static storeRefCount = "__webStore:refCount";
     setItem = (key: string, value: any) => {
         this._data[key] = value;
         localStorage.setItem(WebStore.privateStore, JSON.stringify(this._data));
@@ -36,20 +35,6 @@ export class WebStore extends EventEmitter<{ "webStore:set": { prop: string, new
     }
 
     get currentData() { return this._data }
-    private addToRef = (count = 1) => {
-        let refStr = localStorage.getItem(WebStore.storeRefCount);
-        if (refStr == null) {
-            localStorage.setItem(WebStore.storeRefCount, count.toString());
-            debuglog(`【store】：change ref to ${count}`);
-
-            return count;
-        } else {
-            let refCount: number = JSON.parse(refStr) + count;
-            localStorage.setItem(WebStore.storeRefCount, refCount.toString());
-            debuglog(`【store】：change ref to ${refCount}`);
-            return refCount;
-        }
-    }
 
     constructor(data = {}) {
         super();
@@ -89,23 +74,9 @@ export class WebStore extends EventEmitter<{ "webStore:set": { prop: string, new
                     debuglog(`【store】：其他界面remove ${ev.newValue}`, ev);
                     let key = JSON.parse(ev.newValue);
                     this._data[key] = undefined;
-                } else if (ev.key == WebStore.storeRefCount) {
-                    debuglog(`【store】：refCount= ${localStorage.getItem(WebStore.storeRefCount)}`, ev);
                 }
             }
         });
-        this.addToRef(1);
-        window.addEventListener('beforeunload', () => {
-            let currentCount = this.addToRef(-1);
-            if (currentCount <= 0) {
-                // this.clear();//unload的时候不触发“webStore:set” 事件，直接移除store数据；主动clear的时候才去触发数据修改事件
-                localStorage.removeItem(WebStore.privateStore);
-                localStorage.removeItem(WebStore.storeSetEvent);
-                localStorage.removeItem(WebStore.storeRemoveEvent);
-
-                localStorage.removeItem(WebStore.storeRefCount);
-            }
-        }, false);
     }
     static raiseEvent(topic: string, data: any) {
         localStorage.setItem(topic, JSON.stringify(data));
